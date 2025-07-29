18791e503d958c0de595a1696464eadbb1c47bc5
Adam Putman <adam@ptmn.io>
2025-07-29 11:08:17 -0600

    Changed how powerline is handled

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
 
-mv -r $HOME/.config/tmux/tmux-powerline/ $HOME/.config/
+cp -r $HOME/.config/tmux/tmux-powerline/ $HOME/.config/
